83de7bf696e5d51b45e6ddd4b37090535b5c4d31
Evan Porter <evan.porter@dotman.ca>
2017-06-14 18:57:09 -0300

    Added NewsArticle model

--- dotmanca/users/admin.py
+++ dotmanca/users/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import User
+from .models import User, NewsArticle
 
 
 class MyUserChangeForm(UserChangeForm):
@@ -37,3 +37,6 @@ class MyUserAdmin(AuthUserAdmin):
     ) + AuthUserAdmin.fieldsets
     list_display = ('username', 'name', 'is_superuser')
     search_fields = ['name']
+
+
+admin.site.register(NewsArticle)
--- dotmanca/users/models.py
+++ dotmanca/users/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
-@python_2_unicode_compatible
 class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
@@ -17,3 +15,15 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
+
+
+class NewsArticle(models.Model):
+    headline = models.CharField(max_length=70)
+    slug = models.SlugField()
+    published_datetime = models.DateTimeField()
+    published = models.BooleanField()
+    content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.headline
+
