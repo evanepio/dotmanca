19e0ad9f161f93def6c4267f66f4d99209ffcfbe
Evan Porter <evan.porter@dotman.ca>
2017-07-01 14:54:42 -0300

    Initial Gallery implementation.

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'gallery'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<slug>[-\w]+)/$', views.IndexView.as_view(), name='gallery'),
+    url(r'^(?P<slug>[-\w]+)/$', views.GalleryView.as_view(), name='gallery'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,14 @@
 from django.views import generic
 
-from .models import Gallery
+from .models import Gallery, GalleryImage
 
 
 class IndexView(generic.ListView):
     model = Gallery
     template_name = 'gallery/index.html'
     context_object_name = 'galleries'
+
+
+class GalleryView(generic.DetailView):
+    model = Gallery
+    template_name = 'gallery/gallery.html'
