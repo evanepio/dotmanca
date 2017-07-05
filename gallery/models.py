81cf7c6e349da17ead048ac6f5b4375ef2f377e4
Evan Porter <evan.porter@dotman.ca>
2017-07-05 17:44:12 -0300

    No more left behind images when updating

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_init, post_save
+from django.dispatch import receiver
 from django.urls import reverse
 
 
@@ -41,3 +43,15 @@ class GalleryImage(models.Model):
 
     class Meta:
         unique_together = ('gallery', 'slug')
+
+
+@receiver(post_init, sender=GalleryImage)
+def backup_image_path(sender, instance, **kwargs):
+    instance._current_image_file = instance.the_image
+
+
+@receiver(post_save, sender=GalleryImage)
+def delete_old_image(sender, instance, **kwargs):
+    if hasattr(instance, '_current_image_file'):
+        if instance._current_image_file != instance.the_image.path:
+            instance._current_image_file.delete(save=False)
