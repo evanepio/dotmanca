ffc083f9d1d33b1e88247caf9ff3b77a2eeb8d45
Evan Porter <evan.porter@dotman.ca>
2017-12-20 22:50:50 -0400

    get_context_data test for 2 views, refactored to allow easy testing

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,63 @@
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from .views import AboutChasView, AboutEvanView
+
+
+class TestAboutChasViewGetContextData(TestCase):
+    def test_context_contains_age(self):
+        view = AboutChasView()
+
+        context = view.get_context_data()
+
+        self.assertTrue('age' in context.keys())
+
+    def test_age_ten_years_after_birthday_is_ten(self):
+        view = AboutChasView()
+
+        # Inject the method used to get current date
+        view.date_function = lambda: datetime.date(1990, 4, 21)
+
+        context = view.get_context_data()
+
+        self.assertEquals(10, context['age'])
+
+    def test_age_ten_years_minus_one_day_after_birthday_is_nine(self):
+        view = AboutChasView()
+
+        # Inject the method used to get current date
+        view.date_function = lambda: datetime.date(1990, 4, 20)
+
+        context = view.get_context_data()
+
+        self.assertEquals(9, context['age'])
+
+
+class TestAboutEvanViewGetContextData(TestCase):
+    def test_context_contains_age(self):
+        view = AboutEvanView()
+
+        context = view.get_context_data()
+
+        self.assertTrue('age' in context.keys())
+
+    def test_age_ten_years_after_birthday_is_ten(self):
+        view = AboutEvanView()
+
+        # Inject the method used to get current date
+        view.date_function = lambda: datetime.date(1990, 12, 6)
+
+        context = view.get_context_data()
+
+        self.assertEquals(10, context['age'])
+
+    def test_age_ten_years_minus_one_day_after_birthday_is_nine(self):
+        view = AboutEvanView()
+
+        # Inject the method used to get current date
+        view.date_function = lambda: datetime.date(1990, 12, 5)
+
+        context = view.get_context_data()
+
+        self.assertEquals(9, context['age'])
--- main/views.py
+++ main/views.py
@@ -5,19 +5,18 @@ from django.views import generic
 from news.models import NewsArticle
 
 
-def get_age(birth_year, birth_month, birth_day):
-    today = datetime.date.today()
+def get_age(birthdate, today):
     try:
-        birthday = datetime.date(today.year, birth_month, birth_day)
+        birthday = datetime.date(today.year, birthdate.month, birthdate.day)
     except ValueError:
         # Raised when person was born on 29 February and the current
         # year is not a leap year.
-        birthday = datetime.date(today.year, birth_month, birth_day - 1)
+        birthday = datetime.date(today.year, birthdate.month, birthdate.day - 1)
 
     if birthday > today:
-        return today.year - birth_year - 1
+        return today.year - birthdate.year - 1
     else:
-        return today.year - birth_year
+        return today.year - birthdate.year
 
 
 class HomePageView(generic.TemplateView):
@@ -35,17 +34,23 @@ class AboutView(generic.TemplateView):
 
 class AboutChasView(generic.TemplateView):
     template_name = "main/about_chas.html"
+    date_function = datetime.date.today
 
     def get_context_data(self, **kwargs):
         context = super(AboutChasView, self).get_context_data(**kwargs)
-        context['age'] = get_age(1980, 4, 21)
+        birthdate = datetime.date(1980, 4, 21)
+        today = self.date_function()
+        context['age'] = get_age(birthdate, today)
         return context
 
 
 class AboutEvanView(generic.TemplateView):
     template_name = "main/about_evan.html"
+    date_function = datetime.date.today
 
     def get_context_data(self, **kwargs):
         context = super(AboutEvanView, self).get_context_data(**kwargs)
-        context['age'] = get_age(1980, 12, 6)
+        birthdate = datetime.date(1980, 12, 6)
+        today = self.date_function()
+        context['age'] = get_age(birthdate, today)
         return context
