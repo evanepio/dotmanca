ae5f49162ee73d672f57b40cb1c4ab433d527aa4
Evan Porter <evan.porter@dotman.ca>
2018-01-09 19:54:09 -0400

    Gallery slug and GalleryImage slug in url for a GalleryImage

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Gallery
+from .models import Gallery, GalleryImage
 
 
 class GalleryGetAbsoluteUrl(TestCase):
@@ -12,3 +12,35 @@ class GalleryGetAbsoluteUrl(TestCase):
         result = sut.get_absolute_url()
 
         self.assertTrue(slug_value in result)
+
+
+class GalleryImageGetAbsoluteUrl(TestCase):
+    def test_slug_appears_in_url(self):
+        slug_value = "slug-value"
+
+        gallery = Gallery()
+        gallery.slug = "dont-care"
+
+        sut = GalleryImage()
+
+        sut.slug = slug_value
+        sut.gallery = gallery
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(slug_value in result)
+
+    def test_gallery_slug_appears_in_url(self):
+        gallery_slug = "gallery-slug"
+
+        gallery = Gallery()
+        gallery.slug = gallery_slug
+
+        sut = GalleryImage()
+
+        sut.slug = "dont-care"
+        sut.gallery = gallery
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(gallery_slug in result)
