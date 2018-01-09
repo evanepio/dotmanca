afbea9883c35bbcb5598adc31be963fd377e38fa
Evan Porter <evan.porter@dotman.ca>
2018-01-09 19:46:46 -0400

    Place slug in url for a Place

--- places/tests.py
+++ places/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Place
+
+
+class PlaceGetAbsoluteUrl(TestCase):
+    def test_slug_appears_in_url(self):
+        slug_value = "slug_value"
+        sut = Place()
+        sut.slug = slug_value
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(slug_value in result)
