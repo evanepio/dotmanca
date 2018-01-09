1ee35a355b9895c3c4f89937db593811cf47ba4c
Evan Porter <evan.porter@dotman.ca>
2018-01-09 19:50:24 -0400

    Gallery slug in url for a Gallery

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Gallery
+
+
+class GalleryGetAbsoluteUrl(TestCase):
+    def test_slug_appears_in_url(self):
+        slug_value = "slug_value"
+        sut = Gallery()
+        sut.slug = slug_value
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(slug_value in result)
