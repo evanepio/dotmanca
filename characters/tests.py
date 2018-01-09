4f0c962bb6e44fdde9916c45825c046fadb3dd98
Evan Porter <evan.porter@dotman.ca>
2018-01-09 19:36:02 -0400

    Character slug in url for a Character

--- characters/tests.py
+++ characters/tests.py
@@ -3,10 +3,27 @@ from django.test import TestCase
 from .models import Character, Team
 
 class TeamGetAbsoluteUrl(TestCase):
+    def test_slug_appears_in_url(self):
+        slug_value = "slug-value"
+
+        team = Team()
+        team.slug = "dont-care"
+
+        sut = Character()
+
+        sut.slug = slug_value
+        sut.team = team
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(slug_value in result)
+
     def test_team_slug_appears_in_url(self):
         team_slug = "team-slug"
+
         team = Team()
         team.slug = team_slug
+
         sut = Character()
 
         sut.slug = "dont-care"
