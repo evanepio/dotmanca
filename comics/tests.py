bdd7f53e22103dbed4d6a75ee01d0224dbb2ce5c
Evan Porter <evan.porter@dotman.ca>
2018-03-13 22:54:46 -0300

    Add basic tests

--- comics/tests.py
+++ comics/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+
+from .models import Issue, Arc
+
+
+class IssueGetAbsoluteUrl(TestCase):
+    def test_slug_appears_in_url(self):
+        slug_value = "slug_value"
+
+        arc = Arc()
+        arc.slug = "dont-care"
+
+        sut = Issue()
+        sut.slug = slug_value
+        sut.arc = arc
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(slug_value in result)
+
+    def test_arc_slug_appears_in_url(self):
+        arc_slug = "arc-slug"
+
+        arc = Arc()
+        arc.slug = arc_slug
+
+        sut = Issue()
+
+        sut.slug = "dont-care"
+        sut.arc = arc
+
+        result = sut.get_absolute_url()
+
+        self.assertTrue(arc_slug in result)
