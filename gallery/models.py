24151ca5d19c3743854cacf8bccb1709a7d46a87
Evan Porter <evan.porter@dotman.ca>
2018-09-30 19:38:45 -0300

    Format using  formatting tool

--- gallery/models.py
+++ gallery/models.py
@@ -12,19 +12,19 @@ class Gallery(models.Model):
     sort_order = models.IntegerField()
 
     def get_absolute_url(self):
-        kwargs = {'slug': self.slug}
-        return reverse('gallery:gallery', kwargs=kwargs)
+        kwargs = {"slug": self.slug}
+        return reverse("gallery:gallery", kwargs=kwargs)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'galleries'
+        verbose_name_plural = "galleries"
 
 
 def gallery_image_upload_to(instance, file_name):
     file_extension = file_name.split(".")[-1]
-    return F'galleries/{instance.gallery.slug}/{instance.slug}.{file_extension}'
+    return f"galleries/{instance.gallery.slug}/{instance.slug}.{file_extension}"
 
 
 class GalleryImage(models.Model):
@@ -33,17 +33,21 @@ class GalleryImage(models.Model):
     slug = models.SlugField()
     sort_order = models.IntegerField()
 
-    the_image = models.ImageField(null=False, blank=False, upload_to=gallery_image_upload_to,
-                                  storage=OverwriteStorage())
+    the_image = models.ImageField(
+        null=False,
+        blank=False,
+        upload_to=gallery_image_upload_to,
+        storage=OverwriteStorage(),
+    )
     added_timestamp = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
 
     def get_absolute_url(self):
-        kwargs = {'gallery_slug': self.gallery.slug, 'slug': self.slug}
-        return reverse('gallery:gallery_image', kwargs=kwargs)
+        kwargs = {"gallery_slug": self.gallery.slug, "slug": self.slug}
+        return reverse("gallery:gallery_image", kwargs=kwargs)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        unique_together = ('gallery', 'slug')
+        unique_together = ("gallery", "slug")
