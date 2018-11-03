ece695a67f888650a25c07214faa1b40796e05ee
Evan Porter <evan.porter@dotman.ca>
2018-11-03 16:33:58 -0300

    Gallery navigation

--- gallery/views.py
+++ gallery/views.py
@@ -25,3 +25,45 @@ class GalleryImageView(generic.DetailView):
             .filter(gallery__slug=self.kwargs.get("gallery_slug"))
         )
         return query_set
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        gallery_id = self.object.gallery_id
+        sort_order = self.object.sort_order
+
+        context["previous"] = get_previous_image(gallery_id, sort_order)
+        context["next"] = get_next_image(gallery_id, sort_order)
+
+        return context
+
+
+def get_previous_image(gallery_id, sort_order):
+    previous = None
+
+    try:
+        previous = (
+            GalleryImage.objects.filter(gallery_id=gallery_id)
+            .filter(sort_order__lt=sort_order)
+            .order_by("sort_order")[0]
+        )
+    except IndexError:
+        pass  # Don't worry about it
+
+    return previous
+
+
+def get_next_image(gallery_id, sort_order):
+    previous = None
+
+    try:
+        previous = (
+            GalleryImage.objects.filter(gallery_id=gallery_id)
+            .filter(sort_order__gt=sort_order)
+            .order_by("sort_order")[0]
+        )
+    except IndexError:
+        pass  # Don't worry about it
+
+    return previous
+
