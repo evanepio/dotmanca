da29fcca821e293ad7856e87f747817e4863a27f
Evan Porter <evan.porter@dotman.ca>
2018-11-03 17:54:55 -0300

    Not quite there, but have comic page links showing up.

--- comics/urls.py
+++ comics/urls.py
@@ -6,4 +6,9 @@ app_name = "comics"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<slug:arc_slug>/<slug:slug>/", views.IssueView.as_view(), name="issue"),
+    path(
+        "<slug:arc_slug>/<slug:issue_slug>/<slug:slug>/",
+        views.ComicPageView.as_view(),
+        name="comic_page",
+    ),
 ]
--- comics/views.py
+++ comics/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 
+from gallery.models import GalleryImage
 from .models import Arc, Issue
 
 
@@ -12,3 +13,14 @@ class IndexView(generic.ListView):
 class IssueView(generic.DetailView):
     model = Issue
     template_name = "comics/issue.html"
+
+
+class ComicPageView(generic.DetailView):
+    model = GalleryImage
+    template_name = "comics/comic_page.html"
+
+    def get_queryset(self):
+        query_set = (
+            super().get_queryset().filter(issue__slug=self.kwargs.get("issue_slug"))
+        )
+        return query_set
