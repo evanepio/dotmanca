f022acbd0ee16340a7ed8dc77e5535e3d986d37a
Evan Porter <evan.porter@dotman.ca>
2018-11-03 18:23:47 -0300

    Basic comis page working

--- comics/views.py
+++ comics/views.py
@@ -24,7 +24,10 @@ class ComicPageView(generic.DetailView):
     template_name = "comics/comic_page.html"
 
     def get_queryset(self):
-        query_set = (
-            super().get_queryset().filter(issue__slug=self.kwargs.get("issue_slug"))
+        # Find Issue, then get gallery
+        issue = Issue.objects.filter(arc__slug=self.kwargs.get("arc_slug")).get(
+            slug=self.kwargs.get("issue_slug")
         )
+
+        query_set = super().get_queryset().filter(gallery__id=issue.gallery.id)
         return query_set
