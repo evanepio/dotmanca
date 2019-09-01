4e86a80cddddf00e7c2ee6310de94604fa89c109
Evan Porter <evan.porter@dotman.ca>
2019-09-01 18:26:07 -0300

    Adding /thegrams/ URL and javascript block

--- config/urls.py
+++ config/urls.py
@@ -7,36 +7,44 @@ from django.views import defaults as default_views
 from main.views import HomePageView
 
 urlpatterns = [
-    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r"^$", HomePageView.as_view(), name="home"),
     # url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
-
     # User management
-    url(r'^users/', include('dotmanca.users.urls', namespace='users')),
-
+    url(r"^users/", include("dotmanca.users.urls", namespace="users")),
     # Your stuff: custom urls includes go here
-    url(r'^news/', include('news.urls')),
-    url(r'^about/', include('main.urls')),
-    url(r'^galleries/', include('gallery.urls')),
-    url(r'^comics/', include('comics.urls')),
-    url(r'^characters/', include('characters.urls')),
-    url(r'^places/', include('places.urls')),
-
+    url(r"^news/", include("news.urls")),
+    url(r"^about/", include("main.urls")),
+    url(r"^galleries/", include("gallery.urls")),
+    url(r"^comics/", include("comics.urls")),
+    url(r"^characters/", include("characters.urls")),
+    url(r"^places/", include("places.urls")),
+    url(r"^thegrams/", include("thegrams.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
+        url(
+            r"^400/$",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        url(
+            r"^403/$",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        url(
+            r"^404/$",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        url(r"^500/$", default_views.server_error),
     ]
-    if 'debug_toolbar' in settings.INSTALLED_APPS:
+    if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
-        urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
-        ] + urlpatterns
+
+        urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
