d2fe94110635b1223dbe57ac83d8600d101c1772
Evan Porter <evan.porter@dotman.ca>
2019-09-01 18:25:13 -0300

    Example instagram app

---
+++ thegrams/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ThegramsConfig(AppConfig):
+    name = 'thegrams'
---
+++ thegrams/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+app_name = "thegrams"
+
+urlpatterns = [path("", views.IndexView.as_view(), name="index")]
