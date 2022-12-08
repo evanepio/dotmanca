8e3de6c02e4a022feb94653671e53f19e9c641b1
Evan Porter <evan.porter@dotman.ca>
2022-12-07 22:24:27 -0400

    Convert most of the user tests to pytest

--- dotmanca/users/tests/test_admin.py
+++ dotmanca/users/tests/test_admin.py
@@ -1,20 +1,6 @@
-import uuid
-
 import pytest
 
 from ..admin import MyUserCreationForm
-from ..models import User
-
-
-@pytest.fixture
-def create_user(db):
-    def make_user(**kwargs):
-        kwargs["password"] = "DigiSuperCowNinja123!"
-        if "username" not in kwargs:
-            kwargs["username"] = str(uuid.uuid4())
-        return User.objects.create_user(**kwargs)
-
-    return make_user
 
 
 @pytest.mark.django_db
