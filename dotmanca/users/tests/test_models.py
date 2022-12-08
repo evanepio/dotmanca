4e1029a03bc42d4156450a1f0db77b5d461e3327
Evan Porter <evan.porter@dotman.ca>
2022-12-07 22:25:13 -0400

    Fix formatting issues

--- dotmanca/users/tests/test_models.py
+++ dotmanca/users/tests/test_models.py
@@ -2,6 +2,7 @@ def test__str__(create_user):
     user = create_user(username="testuser", email=None)
     assert user.__str__() == "testuser"
 
+
 def test_get_absolute_url(create_user):
     user = create_user(username="testuser", email=None)
     assert user.get_absolute_url() == "/users/testuser/"
--- dotmanca/users/tests/test_urls.py
+++ dotmanca/users/tests/test_urls.py
@@ -5,29 +5,36 @@ def test_user_url_list_reverse():
     """users:list should reverse to /users/."""
     assert reverse("users:list"), "/users/"
 
+
 def test_user_url_list_resolve():
     """/users/ should resolve to users:list."""
     assert resolve("/users/").view_name, "users:list"
+
 
 def test_user_url_redirect_reverse():
     """users:redirect should reverse to /users/~redirect/."""
     assert reverse("users:redirect"), "/users/~redirect/"
 
+
 def test_user_url_redirect_resolve():
     """/users/~redirect/ should resolve to users:redirect."""
     assert resolve("/users/~redirect/").view_name, "users:redirect"
+
 
 def test_user_url_detail_reverse():
     """users:detail should reverse to /users/testuser/."""
     assert reverse("users:detail", kwargs={"username": "testuser"}), "/users/testuser/"
 
+
 def test_user_url_detail_resolve():
     """/users/testuser/ should resolve to users:detail."""
     assert resolve("/users/testuser/").view_name, "users:detail"
 
+
 def test_user_url_update_reverse():
     """users:update should reverse to /users/~update/."""
     assert reverse("users:update"), "/users/~update/"
+
 
 def test_user_url_update_resolve():
     """/users/~update/ should resolve to users:update."""
