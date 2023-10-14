7a21484055d239b980da67cb039bff9662510339
Evan Porter <evan.porter@dotman.ca>
2023-10-14 16:21:06 -0300

    Remove dumb debug statement.
    Woof.

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -179,7 +179,6 @@ TEMPLATES = [
 USE_S3 = env.bool("USE_S3", default=False)
 
 if USE_S3:
-    print("****************** USE S3 ******************")
     # aws settings
     AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
