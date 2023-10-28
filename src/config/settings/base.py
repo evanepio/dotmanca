2962559032b1ac20a515b15bef363d7f315fc8aa
Evan Porter <evan.porter@dotman.ca>
2023-10-28 12:22:52 -0300

    Fix reading of .env file

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -24,9 +24,7 @@ if READ_DOT_ENV_FILE:
     # Operating System Environment variables have precedence over variables defined in the .env file,
     # that is to say variables from the .env files will only be used if not defined
     # as environment variables.
-    env_file = str(ROOT_DIR.path(".env"))
-    print("Loading : {}".format(env_file))
-    env.read_env(env_file)
+    environ.Env().read_env(".env")
     print("The .env file has been loaded. See base.py for more information")
 
 # APP CONFIGURATION
