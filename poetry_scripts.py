ca776bff7b4a9d618dea79aed78e95fd528a0f9e
Evan Porter <evan.porter@dotman.ca>
2023-10-28 12:23:50 -0300

    Add poetry scripts, update lock file

---
+++ poetry_scripts.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import sys
+
+from django.core.management import execute_from_command_line
+
+
+def _execute(*sys_args):
+    os.environ.setdefault("DJANGO_READ_DOT_ENV_FILE", "True")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    sys.argv = sys_args
+    execute_from_command_line(sys.argv)
+
+
+def run_server():
+    """Run django build in server for local development"""
+    _execute("manage.py", "runserver", "8080")
+
+
+def run_lint():
+    """Run `flake8` for linting"""
+    result = subprocess.run("flake8")
+    exit(result.returncode)
+
+
+def run_check_format():
+    """Run `black` to check if the code is pretty"""
+    result = subprocess.run(["black", "--check", "--diff", "."])
+    exit(result.returncode)
+
+
+def run_auto_format():
+    """Run `black` to make the code pretty"""
+    result = subprocess.run(["black", "."])
+    exit(result.returncode)
+
+
+def manage():
+    _execute("manage.py", *sys.argv[1:])
